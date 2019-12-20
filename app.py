4d4cbbb6112fb95c5045719878aebb51f4461c29
Rico <rui_wang0508@163.com>
2019-12-20 14:18:58 +0800

    Add homepage

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return '<h1>Hello Rico!</h1><image src="http://wx4.sinaimg.cn/mw690/006HJgYYgy1fsnwktpl2fg305k05k49y.gif">'
+
+
+@app.route('/user/<name>')
+def user_page(name):
+    return 'Hello %s' % name
 
 
 if __name__ == '__main__':
